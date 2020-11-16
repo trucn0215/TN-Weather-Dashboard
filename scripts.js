3f031010df2270937abfcdfb119e453f988d93d0
Truc Nguyen <trucn0215@gmail.com>
2020-11-15 23:19:57 -0800

    got UV Index value and display on browser

--- scripts.js
+++ scripts.js
@@ -19,51 +19,55 @@ searchBtnEl.on("click", function () {
     makeWeatherRequest(cityName);
 })
 
-// Create EventListenner.
-
-// function handleSearch() {
-
-// makeWeatherRequest(search);
-// }
-
-// THEN I get the value that the entered into the search input
-
 // GET data from Openweathermap with ajax
 function makeWeatherRequest(cityName) {
-    // Get city value input
-    // var cityName = cityEl.val().toLowerCase();
+    // Get weather values
     var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
         console.log(response)
-    
+
         currentWeather(response);
     })
-
-    // $.ajax(queryUrl).then(function (response) {
-    // THEN get the lat and lng out of the `response` object
-    // NEXT call `makeOneCallRequest( lat, lng )` and pass in the lat and lng;
-
-    // });
 }
 
 // Function to display the TODAY data-time  weather
-function currentWeather(response){
+function currentWeather(response) {
     var cityValue = response.name;
     var iconValue = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png").addClass("bg-primary rounded");
-    var tempValue = (response.main.temp -273.15) * 1.8 +32;
+    var tempValue = (response.main.temp - 273.15) * 1.8 + 32;
     var humidityValue = response.main.humidity;
     var windSpeedValue = response.wind.speed;
 
-    console.log();
-    
-    cityEl.text((cityValue) + " (" + todayDate + " )");
+    // Get lat and lon from response
+    var latValue = response.coord.lat;
+    var lonValue = response.coord.lon;
+
+    // console.log(latValue);
+    // console.log(lonValue);
+
+    cityEl.text((cityValue) + " (" + todayDate + " ) ");
     cityEl.append(iconValue);
     temperatureEl.text("Temperature: " + tempValue.toFixed(1) + " °F");
     humidityEl.text("Humidity: " + humidityValue);
     windspeedEl.text("Wind Speed: " + windSpeedValue);
+
+    uvIndexWeather(latValue, lonValue);
+
+
+}
+
+function uvIndexWeather(latValue, lonValue) {
+    // Get UV INDEX value
+    var uvIndexQueryUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latValue + "&lon=" + lonValue + "&appid=" + apiKey;
+    $.ajax({
+        url: uvIndexQueryUrl,
+        method: "GET"
+    }).then(function (uvData) {
+        uvIndexEl.text("UV Index: " + uvData.value);
+    });
 }
 
 // function makeOneCallRequest(lat, lng) {
